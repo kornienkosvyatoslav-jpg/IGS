7412b7ab243b4cd7b2b6d101a3a48f19a6892990
kornienkosvyatoslav-jpg <kornienko.svyatoslav@gmail.com>
2026-04-23 18:48:03 -0600

    igs_tabbar.lua

--- addons/igs-core/lua/igs/interface/vgui/igs_tabbar.lua
+++ addons/igs-core/lua/igs/interface/vgui/igs_tabbar.lua
@@ -1,32 +1,23 @@
+-- IGS Tabbar — горизонтальний зверху
 local PANEL = {}
-
-local barTall = 50
-local btnWide = 70
+local TAB_H = 40
 
 function PANEL:Init()
-	self.activity = uigs.Create("igs_multipanel",self)
-
-	self.tabBar = uigs.Create("Panel",self)
-	self.tabBar:SetTall(barTall)
+	self.activity = uigs.Create("igs_multipanel", self)
+	self.tabBar   = uigs.Create("Panel", self)
+	self.tabBar:SetTall(TAB_H)
 	self.tabBar.Paint = function(_, w, h)
-		surface.SetDrawColor(IGS.col.TAB_BAR)
-		surface.DrawRect(0,0,w,h) -- bg
-
+		draw.RoundedBox(0, 0, 0, w, h, IGS.col.TAB_BAR)
 		surface.SetDrawColor(IGS.col.HARD_LINE)
-		surface.DrawLine(0,0,w,0) -- upper line
+		surface.DrawLine(0, h - 1, w, h - 1)
 	end
-
-	self.btnsPan = uigs.Create("DIconLayout", self.tabBar)
-	self.btnsPan.Paint = function() end
-
 	self.Buttons = {}
 end
 
 function PANEL:SetActiveTab(num)
-	for i,btn in ipairs(self.Buttons) do
-		btn.Active = num == i -- для подсветки
+	for i, btn in ipairs(self.Buttons) do
+		btn.Active = (num == i)
 	end
-
 	self.activity:SetActivePanel(num)
 end
 
@@ -34,78 +25,62 @@ function PANEL:GetActiveTab()
 	return self.activity:GetActivePanel()
 end
 
-function PANEL:AddTab(sTitle,panel,sIcon,bActive)
-	local ID = self.activity:AddPanel(panel,bActive)
+function PANEL:AddTab(sTitle, panel, sIcon, bActive)
+	local ID = self.activity:AddPanel(panel, bActive)
 
-	local button = uigs.Create("DButton", function(btn)
-		btn:SetSize(btnWide, 50)
-		btn:SetText("")
-
-		btn:SetFont("igs.24")
-
-		btn.DoClick = function(s)
-			self:SetActiveTab(s.ID)
+	local btn = uigs.Create("DButton", function(s)
+		s:SetSize(80, TAB_H)
+		s:SetText("")
+		s.DoClick = function(ss)
+			self:SetActiveTab(ss.ID)
 		end
-
-		--[[-------------------------------------------------------------------------
-			TODO Сделать отрисовку скина через скин хук
-			чтобы можно было юзать компонент не только в IGS без порчи дизайна
-			В bar.Paint тоже
-		---------------------------------------------------------------------------]]
-		btn.Paint = function(s,w,h)
-			if s.Active then
+		s.Paint = function(ss, w, h)
+			if ss.Active then
 				surface.SetDrawColor(IGS.col.HIGHLIGHTING)
+				surface.DrawRect(0, h - 2, w, 2)
 				surface.SetTextColor(IGS.col.HIGHLIGHTING)
+			elseif ss.Hovered then
+				surface.SetTextColor(IGS.col.TEXT_HARD)
 			else
-				surface.SetDrawColor(IGS.col.HIGHLIGHT_INACTIVE)
 				surface.SetTextColor(IGS.col.HIGHLIGHT_INACTIVE)
 			end
-
-			if sIcon then
-				surface.SetMaterial( Material(sIcon) )
-				surface.DrawTexturedRect(w / 2 - 32 / 2,3,32,32)
-			end
-
-			if sTitle then
-				surface.SetFont("igs.15")
-
-				local tw = surface.GetTextSize(sTitle)
-				surface.SetTextPos(w / 2 - tw / 2,32 + 3)
-
-				surface.DrawText( sTitle )
-			end
+			surface.SetFont("igs.18")
+			local tw = surface.GetTextSize(sTitle)
+			if tw + 32 ~= w then s:SetWide(tw + 32) end
+			surface.SetTextPos(w / 2 - tw / 2, h / 2 - 8)
+			surface.DrawText(sTitle)
 		end
-
-		btn.ID     = ID
-		btn.Tab    = panel
-		btn.Active = bActive
+		s.ID     = ID
+		s.Active = bActive
 	end)
 
-	function button:Name()
-		return sTitle
+	function btn:Name() return sTitle end
+
+	-- Додаємо кнопку в таббар
+	btn:SetParent(self.tabBar)
+	table.insert(self.Buttons, btn)
+
+	-- Перераховуємо позиції
+	local x = 16
+	for _, b in ipairs(self.Buttons) do
+		b:SetPos(x, 0)
+		x = x + b:GetWide()
 	end
 
-	self.btnsPan:Add(button)
-	table.insert(self.Buttons, button)
-
-	-- self:PerformLayout()
-	self.btnsPan:SetSize(#self.Buttons * btnWide,barTall)
-	self.btnsPan:SetPos((self:GetWide() - self.btnsPan:GetWide()) / 2)
-
-
-	return button
+	return btn
 end
 
-function PANEL:PerformLayout()
-	self.tabBar:SetWide(self:GetWide())
-	self.tabBar:SetPos(0,self:GetTall() - self.tabBar:GetTall())
-
-	self.activity:SetSize(self.tabBar:GetWide(),self:GetTall() - barTall)
+function PANEL:PerformLayout(w, h)
+	-- Таббар зверху
+	self.tabBar:SetSize(w, TAB_H)
+	self.tabBar:SetPos(0, 0)
+	-- Activity під таббаром
+	self.activity:SetPos(0, TAB_H)
+	self.activity:SetSize(w, h - TAB_H)
 end
 
-function PANEL:Paint(w,h)
-	draw.RoundedBox(0,0,0,w,h,IGS.col.ACTIVITY_BG)
+function PANEL:Paint(w, h)
+	draw.RoundedBox(0, 0, 0, w, h, IGS.col.ACTIVITY_BG)
 end
 
 vgui.Register("igs_tabbar", PANEL, "Panel")
--- IGS.UI()
